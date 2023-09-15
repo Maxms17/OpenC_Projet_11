8456b3975449b8248d613471dd668ef8a548654c
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-09-15 11:19:00 +0200

    ajout index

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
+function Layout ({}){
+    return ( 
+        <>
+        <Header />
+        <Footer />
+       </>
+    )
+}
+
+export default Layout
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+import './Home.css'
+
+function Home() {
+    return (
+        <Layout>
+        </Layout>
+    )
+}
+
+export default Home
