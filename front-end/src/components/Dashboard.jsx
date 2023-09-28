9497abeae84e61f3f356f381799cbab7e456546f
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-09-28 14:27:13 +0200

    ajout nom utilisateur au html

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import Account from './Account';
 
 import '../css/main.css';
 
 const Dashboard = () => {
+  const [user, setUser] = useState({ firstName: '', lastName: '' });
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/user/signup')
+      .then(response => response.json())
+      .then(data => {
+        const { firstName, lastName } = data;
+        setUser({ firstName, lastName });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des informations de l\'utilisateur :', error);
+      });
+  }, []);
+
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
+        <h1>Welcome back<br />{user.firstName} {user.lastName}!</h1>
         <button className="edit-button">Edit Name</button>
       </div>
       <h2 className="sr-only">Accounts</h2>
