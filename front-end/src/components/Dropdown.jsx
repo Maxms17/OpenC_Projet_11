7d750c77342abb71e626cbd8bf68d88616463a04
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-10-10 16:07:06 +0200

    modification dropdown finis

--- front-end/src/components/Dropdown.jsx
+++ front-end/src/components/Dropdown.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import jsonData from '../data.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'; 
+import { faChevronUp, faChevronDown, faPencilAlt } from '@fortawesome/free-solid-svg-icons'; 
 import '../css/main.css';
 
 function Dropdown({ id }) {
@@ -39,21 +39,38 @@ function Dropdown({ id }) {
 
       {targetTransaction ? (
         targetTransaction.data.map((transactionData, index) => (
-          <div className='transact_info' key={index}>
-            <div className='date'>
-              <p>{transactionData.date}</p>
+          <div key={index}>
+            <div className='transact_info'>
+              <div className='date'>
+                <p>{transactionData.date}</p>
+              </div>
+              <div className='description'>
+                <p>{transactionData.description}</p>
+              </div>
+              <div className='amount'>
+                <p>{transactionData.amount}</p>
+              </div>
+              <div className='balance'>
+                <p>{transactionData.balance}</p>
+              </div>
+              <div className='fleche' onClick={() => toggleDetails(index)}>
+                <FontAwesomeIcon icon={showDetails[index] ? faChevronUp : faChevronDown} />
+              </div>
             </div>
-            <div className='description'>
-              <p>{transactionData.description}</p>
-            </div>
-            <div className='amount'>
-              <p>{transactionData.amount}</p>
-            </div>
-            <div className='balance'>
-              <p>{transactionData.balance}</p>
-            </div>
-            <div className='fleche' onClick={() => toggleDetails(index)}>
-              <FontAwesomeIcon icon={showDetails[index] ? faChevronUp : faChevronDown} />
+            <div>
+              {showDetails[index] && (
+                <div className='details'>
+                  <p>Transaction Type : {transactionData.type}</p>
+                  <div className='pencil'>
+                    <p>Category : <input type="text" value={transactionData.Category}/></p>
+                    <FontAwesomeIcon icon={faPencilAlt} className='pencil-icon' />
+                  </div>
+                  <div className='pencil'>
+                    <p>Note : <input type="text" value={transactionData.Note}/></p>
+                    <FontAwesomeIcon icon={faPencilAlt} className='pencil-icon' />
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         ))
